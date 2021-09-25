afc6f9019c7ccbcd7071ef85cda0fb445162d1ba
JhoellOpeyemi <jhoellpelumi@gmail.com>
2021-09-26 00:53:16 +0100

    edited paste link input

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,17 @@
 const modal = document.querySelector(".modal-bg");
 const openModalBtn = document.querySelector(".open-modal");
 const closeModalBtn = document.querySelector(".close-modal");
+const modalInput = document.querySelector(".modal-input");
+const pasteLinkInput = document.querySelector(".paste-link-input");
 
-openModalBtn.addEventListener("click", () => {
-  setTimeout(function () {
-    modal.classList.add("active");
-  }, 2000);
+openModalBtn.addEventListener("click", (e) => {
+  if (pasteLinkInput.value != "") {
+    e.preventDefault();
+    setTimeout(function () {
+      modalInput.value = pasteLinkInput.value;
+      modal.classList.add("active");
+    }, 2000);
+  }
 });
 
 closeModalBtn.addEventListener("click", () => {
